4654b988d99ac6fe0c77af9a1d947fcc63084416
Anheru88 <ajimenez@digitalcaribe.com.co>
2014-08-19 14:07:01 -0500

    Se creo el controlador de registro de usuarios

---
+++ app/controllers/RegistrationController.php
@@ -0,0 +1,15 @@
+ <?php
+
+class RegistrationController extends \BaseController {
+
+	/**
+	 * Show the form for register the user.
+	 *
+	 * @return Response
+	 */
+	public function create()
+	{
+		return View::make('registration.crete');
+	}
+
+}
