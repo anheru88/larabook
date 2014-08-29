226770a2b9fe6edf89454990fd1c337a92ff7fbe
Anheru88 <ajimenez@digitalcaribe.com.co>
2014-08-29 17:19:03 -0500

    Uso de Registration Form par el control de excepciones del formulario de regitro

--- app/controllers/RegistrationController.php
+++ app/controllers/RegistrationController.php
@@ -1,6 +1,19 @@
  <?php
 
+ use Larabook\Forms\RegistrationForm;
+
 class RegistrationController extends \BaseController {
+
+	/**
+	 * @var RegistrationForm
+	 */
+	private $registrationForm;
+
+	function __construct(RegistrationForm $registrationForm)
+	{
+		$this->registrationForm = $registrationForm;
+	}
+
 
 	/**
 	 * Show the form for register the user.
@@ -19,6 +32,7 @@ class RegistrationController extends \BaseController {
 	 */
 	public function store()
 	{
+		$this->registrationForm->validate(Input::all());
 
 		$user = User::create(
 			Input::only('username', 'email', 'password')
